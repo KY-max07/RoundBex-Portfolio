afde5aba9cc700ac6f12b4a11669426ecca0305e
koushik <koushikyerraguntla@gmail.com>
2025-07-23 06:33:16 +0530

    minor changes2

--- src/Constants/ProjectsData.js
+++ src/Constants/ProjectsData.js
@@ -5,7 +5,7 @@ export const projects = [
       "A dynamic Task Manager with a modern UI and smooth animations. It allows users to create and delete tasks with an animated burning barrel effect, and supports drag-and-drop functionality to organize tasks. Categorize tasks with ease and enjoy real-time authentication it ensures secure access  — all within a fully responsive design.",
     link: "https://tick-track-rho.vercel.app/",
     src: "/tick-track-web.png",
-    color: "#BBACAF",
+    color: "#DA6C6C",
     tools: ["React", "Supabase", "Clerk", "TailwindCSS"],
   },
   {
@@ -23,7 +23,7 @@ export const projects = [
       "This is an AI-powered Telegram assistant that replies to user messages using OpenAI. It can remember past chats, talk using voice, and search information using Pinecone. It also connects to Google Calendar to check availability, find free time slots, and book appointments with Google Calender API. All messages are sent and received through Telegram.",
     link: "/",
     src: "/n8n.png",
-    color: "#DA6C6C",
+    color: "#BBACAF",
     tools: ["AI-Agents", "Telegram", "ChatGPT", "calender"],
   },
   {
@@ -32,7 +32,7 @@ export const projects = [
       "I built FigureOut, a smart app that combines movie discovery and live weather updates using the TMDB and OpenWeather APIs, offering trending films and real-time forecasts in one place. I also developed a standalone Weather App for accurate, location-based weather data with a clean, responsive UI. Additionally, I created a Twilio WhatsApp Message Scheduler that allows users to schedule and send automated messages, making it perfect for reminders or time-based notifications. Checkout github for more details.",
     link: "https://github.com/KY-max07/",
     src: "/others.png",
-    color: "#FCC8D1",
+    color: "#FF5858",
     tools: ["Weather-App", "|", "FigureOUT-Movie", "|", "more.."],
   },
 ];
