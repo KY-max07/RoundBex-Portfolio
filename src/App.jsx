91c69b442b892c953638543d20ed81862967f4f2
koushik <koushikyerraguntla@gmail.com>
2025-08-13 15:46:50 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,6 @@ import Lenis from "lenis";
 import Header from "./Components/Header";
 import LetsTalk from "./Components/LetsTalk";
 
-
-
-
-
 const App = () => {
   const [count, setCount] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
@@ -65,7 +61,7 @@ const App = () => {
 
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
+      duration: 1.5,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smooth: true,
     });
--- src/Components/LetsTalk.jsx
+++ src/Components/LetsTalk.jsx
@@ -1,23 +1,20 @@
-
-import React from 'react'
-
 const LetsTalk = () => {
-  const scrollToSection = (id) => {
-    const section = document.getElementById(id);
-    if (section) {
-      section.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-  
   return (
-    <div 
-    onClick={() => scrollToSection("contact")}
-    className='px-4 bg-neutral-900/50 backdrop-blur-2xl text-neutral-300  md:rounded-2xl z-100 fixed bottom-1 md:right-6 right-1 flex  items-end  justify-end  py-2 gap-2 font-clash2 hover:bg-neutral-800/30 border border-neutral-800 cursor-pointer hover:scale-103 rounded-xl'>
-
-<h2 className='tracking-wide font-bold no-select   relative text-xs md:textsm '>Get In Touch</h2>
-
+    <div
+      onClick={() =>
+        window.open(
+          "https://tally.so/r/w5a86v",
+          "_blank",
+          "noopener,noreferrer"
+        )
+      }
+      className="px-4 bg-neutral-900/50 backdrop-blur-2xl text-neutral-300  md:rounded-2xl z-100 fixed bottom-1 md:right-6 right-1 flex  items-end  justify-end  py-2 gap-2 font-clash2 hover:bg-neutral-800/30 border border-neutral-800 cursor-pointer hover:scale-103 rounded-xl"
+    >
+      <h2 className="tracking-wide font-bold no-select   relative text-xs md:textsm ">
+        Get In Touch
+      </h2>
     </div>
-  )
-}
+  );
+};
 
-export default LetsTalk
+export default LetsTalk;
